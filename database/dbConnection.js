23b70af0ee0b63fccef171c5d1dc1991f9c33509
Hrithik Kumar Sharma <119428353+Hrithik055@users.noreply.github.com>
2025-07-20 22:04:05 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load .env variables
+dotenv.config();
 
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
       dbName: "HOSPITAL_MANAGEMENT_SYSTEM",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then(() => {
-      console.log("Connected to database!");
+      console.log("✅ Connected to database!");
     })
     .catch((err) => {
-      console.log("Some error occured while connecting to database:", err);
+      console.error("❌ Some error occurred while connecting to database:", err);
     });
 };
